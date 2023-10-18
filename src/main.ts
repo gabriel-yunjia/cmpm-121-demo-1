096e17f1cf1587c843e96eb5f5cd33a68d870b5e
gabriel-yunjia <gli79@ucsc.edu>
2023-10-18 00:37:14 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Gabe's game";
+const gameName = "Gabriel's game";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const Button = document.createElement("button");
+Button.innerHTML = "🍟";
+Button.style.fontSize = "40pt";
+app.append(Button);
