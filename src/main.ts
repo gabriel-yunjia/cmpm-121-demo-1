c958592857e96a902d67a229f903fd1fbe5e94b6
gabriel-yunjia <gli79@ucsc.edu>
2023-10-18 13:01:05 -0700

    Step 5 Complete

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ const gameName = "Gabriel's game";
 
 let counter = 0;
 let lastTimestamp = 0;
-const incrementPerSecond = 1;
+let incrementPerSecond = 1;
 
 document.title = gameName;
 
@@ -14,10 +14,10 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-const Button = document.createElement("button");
-Button.innerHTML = "🍟";
-Button.style.fontSize = "40pt";
-app.append(Button);
+const FriesButton = document.createElement("button");
+FriesButton.innerHTML = "🍟";
+FriesButton.style.fontSize = "40pt";
+app.append(FriesButton);
 
 const counterText = document.createElement("div");
 counterText.innerHTML = ` Fries Eaten :  ${counter} `;
@@ -25,7 +25,7 @@ app.append(counterText);
 
 // step 2: Making counter increment when clicked
 
-Button.addEventListener("click", () => {
+FriesButton.addEventListener("click", () => {
   counter++;
   counterText.innerHTML = `Fries Eaten :  ${counter}`;
 });
@@ -49,3 +49,58 @@ function updateCounter(timestamp: number) {
 }
 
 requestAnimationFrame(updateCounter);
+
+interface Item {
+  name: string;
+  cost: number;
+  rate: number;
+  description: string;
+}
+
+// Create upgrade items
+const upgrades: Item[] = [
+  { name: "😋", cost: 10, rate: 1, description: "Another mouth" },
+];
+
+function createUpgradeButtons() {
+  for (const upgrade of upgrades) {
+    const upgradeButton = document.createElement("button");
+    upgradeButton.innerHTML = `${upgrade.name} (Cost: ${upgrade.cost})<br/>${upgrade.description}`;
+    app.append(upgradeButton);
+
+    // Function to handle the upgrade purchase
+    upgradeButton.addEventListener("click", () => {
+      if (counter >= upgrade.cost) {
+        counter -= upgrade.cost; // Deduct the cost from the counter
+        incrementPerSecond += upgrade.rate; // Increase the growth rate
+        counterText.innerHTML = `Monsters Slain: ${counter.toFixed(2)}`;
+        updateButtonStates();
+      }
+    });
+  }
+}
+
+// activate/deactivate button
+function updateButtonStates() {
+  for (const upgrade of upgrades) {
+    const buttons = document.querySelectorAll("button");
+
+    buttons.forEach((button) => {
+      if (button.textContent.includes(upgrade.name)) {
+        const upgradeButton = button as HTMLButtonElement;
+
+        if (counter < upgrade.cost) {
+          upgradeButton.disabled = true; // Disable the button
+        } else {
+          upgradeButton.disabled = false; // Enable the button
+        }
+      }
+    });
+  }
+}
+
+// Make the buttons
+createUpgradeButtons();
+
+// Update Button Staets
+setInterval(updateButtonStates, 100);
